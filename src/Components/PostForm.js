6a017e57d0c4a3dadd1f1f5ae6919158f485220f
nigelrudolf <mansell.nigel@mac.com>
2020-08-16 19:15:29 -0400

    Changed PostForm.js to function component

--- src/Components/PostForm.js
+++ src/Components/PostForm.js
@@ -1,79 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import CloseButton from './CloseButton';
 import PostButton from './PostButton';
 
-class PostForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            title: '',
-            date: '',
-            content: ''
-        }
+function PostForm({closePostForm, handleClick}) {
+
+    const [userInput, setUserInput] = useState({
+        title: '',
+        date: '',
+        content: '',
+    });
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        const date = new Date().toDateString();
+        setUserInput({
+            ...userInput,
+            [event.target.name]: value,
+            date: date,
+        });        
     }
 
-    handleChange = (event) => {
-        const value = event.target.value;
-        
-        const date = new Date().toDateString();
-        
-        this.setState({
-            [event.target.name]: value,
-            [event.target.name]: value,
-            date: date
-        })
-      }
-      
-    clearInput = () => {
-        this.setState({
+
+    const clearInput = () => {
+        setUserInput({
+            ...userInput,
             title: '',
-            date: '',
-            content: ''
-        })
-    }  
+            content: '',
+        });
+    }
 
-
-
-    render() {
-        const {closePostForm, handleClick} = this.props;
-        return (
-            <div className="modal-wrapper">
-                <div className="Modal">
-                    <div className="ModalHeader">
-                        <CloseButton 
-                            closePostForm={closePostForm}
-                            />
-                    </div>
-                    <form className="PostForm">
-                        <input
-                            value={this.state.title}
-                            onChange={this.handleChange}
-                            name="title"
-                            type="text"
-                            required
-
-                            placeholder="Title"
-                            className="title-input"
+    return (
+        <div className="modal-wrapper">
+            <div className="Modal">
+                <div className="ModalHeader">
+                    <CloseButton 
+                        closePostForm={closePostForm}
                         />
-                        <textarea
-                            value={this.state.content}
-                            onChange={this.handleChange}
-                            name="content"
+                </div>
+                <form className="PostForm">
+                   
+           
+                    <input
+                        value={userInput.title}
+                        onChange={handleChange}
+                        name="title"
+                        type="text"
+                        
 
-                            placeholder="What's your idea"
-                        />
-                    </form>
-                    <div className="ModalFooter">
-                        <PostButton
-                            handleClick={handleClick}
-                            userInput={this.state}
-                            clearInput={this.clearInput}
-                        />
-                    </div>
+                        placeholder="Title"
+                        className="title-input"
+                    />
+                    <textarea
+                        value={userInput.content}
+                        onChange={handleChange}
+                        name="content"
+                        type="text"
+
+                        placeholder="What's your idea"
+                    />
+                </form>
+                <div className="ModalFooter">
+                    <PostButton
+                        handleClick={handleClick}
+                        userInput={userInput}
+
+                        clearInput={clearInput}
+                    />
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default PostForm;
