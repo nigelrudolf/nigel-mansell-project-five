ad34bb84305161af58c31c6d576db82b4bace1f5
nigelrudolf <mansell.nigel@mac.com>
2020-08-16 14:23:50 -0400

    Changed CloseButton.js to function component

--- src/Components/CloseButton.js
+++ src/Components/CloseButton.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const element = <FontAwesomeIcon icon={faTimes} size="2x"/>
 
-class CloseButton extends Component {
-    render() {
-        const {closePostForm} = this.props;
-        return (
-            <button className="CloseButton" onClick={closePostForm}>{element}</button>
-            )
-    }
+function CloseButton ({ closePostForm }) {
+    return <button className="CloseButton" onClick={closePostForm}>{element}</button>;
 }
 
 export default CloseButton;
