610a489158104ab0d6dd099594ad9b3f1c769ab1
nigelrudolf <mansell.nigel@mac.com>
2020-08-16 14:21:51 -0400

    Changed AddPostButton.js to function component

--- src/Components/AddPostButton.js
+++ src/Components/AddPostButton.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 
 const element = <FontAwesomeIcon icon={faPen} size="2x"/>;
 
-class AddPostButton extends Component {
-    render() {
-        const {displayPostForm} = this.props;
-        return (
-                <button className="AddPostButton" onClick={displayPostForm}>{element}</button>
-                )
-        }
+function AddPostButton({ displayPostForm }) {
+        return <button className="AddPostButton" onClick={displayPostForm}>{element}</button>;
 }
 
 export default AddPostButton;
