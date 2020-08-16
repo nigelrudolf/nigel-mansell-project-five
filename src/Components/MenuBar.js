f2a9b517094b45b9a324f3809b14baedd4e2eb28
nigelrudolf <mansell.nigel@mac.com>
2020-08-16 14:31:00 -0400

    Changed MenuBar.js to function component

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddPostButton from './AddPostButton';
 
-class MenuBar extends Component {
-    render() {
-        const {displayPostForm} = this.props;
-        return (
-            <nav className="MenuBar">
-                <AddPostButton 
-                    displayPostForm={displayPostForm}
-                    />
-            </nav>
-        )
-    }
+function MenuBar({displayPostForm}) {
+    return (
+        <nav className="MenuBar">
+            <AddPostButton 
+                displayPostForm={displayPostForm}
+                />
+        </nav>
+    )
 }
 
 export default MenuBar;
