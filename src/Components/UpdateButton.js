25b4b0e9369b2fd8a2023241dd1983d620688f44
nigelrudolf <mansell.nigel@mac.com>
2020-08-16 20:18:48 -0400

    Changed UpdateButton.js to function component

--- src/Components/UpdateButton.js
+++ src/Components/UpdateButton.js
@@ -1,14 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-
-class UpdateButton extends Component {
-    render() {
-        
-        return (
-            <button className="UpdateButton" onClick={(event) => {this.props.handleClick(event, this.props.userInput); this.props.clearInput();}}>Update</button>
-            )
-        }
+function UpdateButton({ handleClick, userInput, clearInput }) {
+    return (
+        <button className="UpdateButton" onClick={(event) => {handleClick(event, userInput); clearInput();}}>Update</button>
+        )
 }
 
 export default UpdateButton;
