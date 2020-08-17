bb835ee025cfb3e3362356200b622d01586fa997
nigelrudolf <mansell.nigel@mac.com>
2020-08-16 20:15:34 -0400

    Changed PostOptions.js to function component

--- src/Components/PostOptions.js
+++ src/Components/PostOptions.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt, faEdit} from '@fortawesome/free-solid-svg-icons';
 
 const trash = <FontAwesomeIcon icon={faTrashAlt} size="1x"/>
 const edit = <FontAwesomeIcon icon={faEdit} size="1x"/>
 
-class PostOptions extends Component {
-    render() {
-        return (
-            <div className="PostOptionsModal">
-                <ul>
-                    <li><button onClick={() => this.props.editPost()}>{edit} Edit</button></li>
-                    <li><button onClick={() => this.props.deletePost()}>{trash} Delete</button></li>
-                </ul>
-            </div>
-            )
-    }
+function PostOptions({ editPost, deletePost }) {
+    return (
+        <div className="PostOptionsModal">
+            <ul>
+                <li><button onClick={() => editPost()}>{edit} Edit</button></li>
+                <li><button onClick={() => deletePost()}>{trash} Delete</button></li>
+            </ul>
+        </div>
+        )
 }
 
 export default PostOptions;
