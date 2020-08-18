b9d2d50f3159a6ba15af197e919dac31439193fa
nigelrudolf <mansell.nigel@mac.com>
2020-08-17 20:13:55 -0400

    Changed App.js to function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import firebase from './Components/firebase';
 import MediaSort from './Components/MediaSort';
 import PostForm from './Components/PostForm';
@@ -6,132 +6,100 @@ import PostCard from './Components/PostCard';
 import MenuBar from './Components/MenuBar';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: [],
-      postFormIsVisible: false,
-      mainPanel: {
-        panelId: ""
-      }
-    }
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [postFormIsVisible, setPostFormIsVisible] = useState(false);
+  const [editPostIsVisible, setEditPostIsVisible] = useState(false);
+  const [panelId, setPanelId] = useState('');
+
+  const displayPostForm = () => {
+    setPostFormIsVisible(true);
   }
 
-  displayPostForm = () => {
-    this.setState({
-      postFormIsVisible: true,
-    });
+  const closePostForm = () => {
+    setPostFormIsVisible(false);
   }
 
-  closePostForm = () => {
-    this.setState({
-      postFormIsVisible: false,
-    });
+  const editPost = (postId) => {
+    console.log(`editing ${postId}`);
+
+    setEditPostIsVisible(true);
   }
 
-  // editPost = (postId) => {
-  //   console.log(`editing ${postId}`);
-
-  //   this.setState({
-  //     editPostIsVisible: true
-  //   })
-  // }
-
-  deletePost = (postId) => {
+  const deletePost = (postId) => {
     const dbRef = firebase.database().ref();
     dbRef.child(postId).remove();
   }  
 
-  handleClick = (event, userInput) => {
+  const handleClick = (event, userInput) => {
     event.preventDefault();
     const dbRef = firebase.database().ref();
     dbRef.push(userInput);
 
-    this.setState({
-      postFormIsVisible: false
-    })
+    setPostFormIsVisible(false);
   }
 
-  handleUpdateClick = (event, userInput) => {
+  const handleUpdateClick = (event, userInput) => {
     event.preventDefault();
     const dbref = firebase.database().ref();
     dbref.update(userInput);
     // firebase.database().ref('/-LYYnjbTOR0Qq03ehjb1').update(settingToChange);
   }
 
-  updateMainPanel = (panelId) => {
-
-    this.setState({
-      mainPanel: {
-        panelId,
-      }
-    })
+  const updateMainPanel = (panelId) => {
+    setPanelId(panelId);
   }
 
-  
-  componentDidMount() {
-    const dbRef = firebase.database().ref();
+  useEffect(() => {
+      const dbRef = firebase.database().ref();
 
-    dbRef.on('value', (response) => {
-      const newState = [];
-      const data = response.val();
+      dbRef.on('value', (response) => {
+        const newState = [];
+        const data = response.val();
 
-      for (let key in data) {
-        newState.push({
-          post: data[key],
-          id: key
-        })
-      }
+        for (let key in data) {
+          newState.push({
+            post: data[key],
+            id: key
+          })
+        }
+        setPosts(newState);
+      });  
+  }, []);
 
+  return (
+    <div className="App App-wrapper">
 
-      this.setState({
-        posts: newState,
-      });
-    });
+      <h1 className="logo">Social Media Planner</h1>
+      <main>
+      <MediaSort />
+      {posts.map((post) => {
+          return (
+            <PostCard
+            key={post.id}
+            title={post.post.title}
+            date={post.post.date}
+            content={post.post.content}
+            deletePost={() => deletePost(post.id)}
+            editPost={() => editPost(post.id)}
+            postId={post.id}
+            mainPanelId={panelId}
+            updateMainPanel={updateMainPanel}
+            />
+          )
+        })}
+      { postFormIsVisible ? <PostForm
+        closePostForm={closePostForm}
+        handleClick={handleClick}
+      /> : null }
+      </main>
     
-  }
+      <MenuBar 
+        displayPostForm={displayPostForm}
+      />
+    </div>
+  );
 
-  componentDidUpdate(){
-    
-  }
-
-
-  render() {
-    
-    return (
-      <div className="App App-wrapper">
-
-        <h1 className="logo">Social Media Planner</h1>
-        <main>
-        <MediaSort />
-        {this.state.posts.map((post) => {
-            return (
-              <PostCard
-              key={post.id}
-              title={post.post.title}
-              date={post.post.date}
-              content={post.post.content}
-              deletePost={() => this.deletePost(post.id)}
-              editPost={() => this.editPost(post.id)}
-              postId={post.id}
-              mainPanelId={this.state.mainPanel.panelId}
-              updateMainPanel={this.updateMainPanel}
-              />
-            )
-          })}
-        { this.state.postFormIsVisible ? <PostForm
-          closePostForm={this.closePostForm}
-          handleClick={this.handleClick}
-        /> : null }
-        </main>
-      
-        <MenuBar 
-          displayPostForm={this.displayPostForm}
-        />
-      </div>
-    );
-  }
 }
 
 export default App;
