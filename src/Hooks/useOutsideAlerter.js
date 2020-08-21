87ed0e15c678d117e714a35117a5a2334ee45a46
nigelrudolf <mansell.nigel@mac.com>
2020-08-20 20:43:23 -0400

    Added useOutsideAlerter to detect clicks outside a component

---
+++ src/Hooks/useOutsideAlerter.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+
+function useOutsideAlerter(ref, callback) {
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                callback();
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [callback, ref]);
+}
+
+export default useOutsideAlerter;
