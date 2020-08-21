f7ddc51d68613c30b5b95d2d03e05a49677cab78
nigelrudolf <mansell.nigel@mac.com>
2020-08-21 18:12:23 -0400

    Added references

--- src/Hooks/useOutsideAlerter.js
+++ src/Hooks/useOutsideAlerter.js
@@ -1,3 +1,7 @@
+// References
+// https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+// https://medium.com/@pitipatdop/little-neat-trick-to-capture-click-outside-with-react-hook-ba77c37c7e82
+// https://www.youtube.com/watch?v=eWO1b6EoCnQ
 import { useEffect } from 'react';
 
 function useOutsideAlerter(ref, callback) {
