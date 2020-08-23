6159c2f76309ad6d237a3ff6ffd1780236f87656
nigelrudolf <mansell.nigel@mac.com>
2020-08-22 21:16:10 -0400

    Removed UpdateButton component

--- src/Components/UpdateButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function UpdateButton({ handleClick, userInput, clearInput }) {
-    return (
-        <button className="UpdateButton" onClick={(event) => {handleClick(event, userInput); clearInput();}}>Update</button>
-        )
-}
-
-export default UpdateButton;
